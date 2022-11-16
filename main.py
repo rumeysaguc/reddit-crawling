92fc6ea859babc51e7e76cafabd919537476e287
Rümeysa <70983764+rumeysaguc@users.noreply.github.com>
2022-11-17 01:13:26 +0300

    bugfix

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from google.protobuf.json_format import MessageToJson
 def crawl(arg1, user_agent=None, client_secret=None, client_id=None, page_limit=None):
     outfile = open('result.json', 'w')
     reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, user_agent=user_agent)
-    hot_posts = reddit.subreddit(arg1).hot(limit=10)
+    hot_posts = reddit.subreddit(arg1).hot(limit=page_limit)
     data = {
         'counts_comment': [],
         'score': [],
